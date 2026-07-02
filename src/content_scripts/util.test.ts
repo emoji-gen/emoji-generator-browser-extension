b47c0afd0ffa76f6122409d2a6f737f3f4248239
Pine Mizune <pinemz@gmail.com>
2026-07-02 21:59:55 +0900

    fix: fix test

--- src/content_scripts/util.test.ts
+++ src/content_scripts/util.test.ts
@@ -14,6 +14,10 @@ import {
   CE_SEARCH_JOINED_TEAMS_DONE,
 } from '../event.js';
 
+afterEach(() => {
+  rs.unstubAllGlobals();
+});
+
 describe('getAppId', () => {
   beforeEach(() => {
     document.body.setAttribute('data-app-id', '12345');
@@ -48,25 +52,22 @@ describe('attach', () => {
 
 describe('listenCustomEvent', () => {
   beforeEach(() => {
-    window.chrome = {
+    const chrome = {
       runtime: {
         sendMessage: function (detail: any, response: (detail: any) => void) {
           response(detail);
         },
       },
     };
-  });
-
-  afterEach(() => {
-    window.chrome = undefined;
+    rs.stubGlobal('chrome', chrome);
   });
 
   test('# can listen custom event', () => {
     let callbackEvent: CustomEvent | null = null;
     document.body.addEventListener(
       CE_SEARCH_JOINED_TEAMS_DONE,
-      (e: CustomEvent) => {
-        callbackEvent = e;
+      (e: Event) => {
+        callbackEvent = e as CustomEvent;
       },
     );
 
@@ -75,7 +76,7 @@ describe('listenCustomEvent', () => {
     const ce = new CustomEvent(CE_SEARCH_JOINED_TEAMS, { detail: 'foo' });
     document.body.dispatchEvent(ce);
 
-    expect(callbackEvent.detail).toEqual({
+    expect((callbackEvent as CustomEvent | null)?.detail).toEqual({
       type: CE_SEARCH_JOINED_TEAMS,
       detail: 'foo',
     });
